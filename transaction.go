458b765acc08912d65af8059962eebf6e63103f8
Evan Huus <eapache@gmail.com>
2013-10-23 15:53:42 -0400

    checkpoint wip

--- transaction.go
+++ transaction.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/gob"
+	"errors"
 )
 
 type TxnInput struct {
@@ -18,4 +23,42 @@ type TxnOutput struct {
 type Transaction struct {
 	Inputs  []TxnInput
 	Outputs []TxnOutput
+}
+
+func (txn *Transaction) Hash() []byte {
+	hasher := sha256.New()
+	encoder := gob.NewEncoder(hasher)
+
+	// transaction hash value does not include signatures (or signing would change the hash,
+	// which would make this impossible) so we save and restore the signature fields
+	tmpSigs := make([][]byte, len(txn.Inputs))
+	for i := range txn.Inputs {
+		tmpSigs[i] = txn.Inputs[i].Signature
+		txn.Inputs[i].Signature = nil
+	}
+
+	encoder.Encode(txn)
+
+	for i := range txn.Inputs {
+		txn.Inputs[i].Signature = tmpSigs[i]
+	}
+
+	return hasher.Sum(nil)
+}
+
+func (txn *Transaction) Sign(w *Wallet) (err error) {
+	hash := txn.Hash()
+
+	for i := range txn.Inputs {
+		privKey := w.Keys[txn.Inputs[i].Key]
+		if privKey == nil {
+			return errors.New("could not sign transaction, missing private key")
+		}
+		txn.Inputs[i].Signature, err = rsa.SignPKCS1v15(rand.Reader, privKey, crypto.SHA256, hash)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
