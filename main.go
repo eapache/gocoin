06ae0931c5875c8f5edc52c93976395ba171a38d
Evan Huus <eapache@gmail.com>
2013-10-25 15:03:09 -0400

    checkpoint wip

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 func main() {
+	initialPeer := flag.String("connect", "", "Address of peer, leave blank for new network")
 	flag.Parse()
 
-	network, err := NewPeerNetwork("")
+	network, err := NewPeerNetwork(*initialPeer)
 	if err != nil {
 		panic(err)
 	}
 
-        doUI()
+	fmt.Println("Startup complete, listening on ", network.server.Addr())
+
+	doUI()
 
 	network.Close()
 }
